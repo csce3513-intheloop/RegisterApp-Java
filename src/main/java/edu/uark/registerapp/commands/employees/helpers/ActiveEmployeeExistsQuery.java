445f18a76ced5fa568d5c40c86ccd95dfc0cc382
Corey <coreypogue@corey.ddns.uark.edu>
2020-02-20 13:58:10 -0600

    Added Active Employee Query class

---
+++ src/main/java/edu/uark/registerapp/commands/employees/helpers/ActiveEmployeeExistsQuery.java
@@ -0,0 +1,22 @@
+package edu.uark.registerapp.commands.employees.helpers;
+
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.models.api.Employee;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+public class  ActiveEmployeeExistsQuery implements ResultCommandInterface<ActiveUserEntity> {
+    @Override
+    public ActiveUserEntity execute() {
+        this.validateProperties();
+        
+    }
+}
