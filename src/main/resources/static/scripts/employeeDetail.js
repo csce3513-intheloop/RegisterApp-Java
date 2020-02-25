9ba7a8e84ab72e6369539142f37f4c4dc6000402
Erik Newton <eriknewt@gmail.com>
2020-02-25 15:12:37 -0600

    employeeDetail.js

--- src/main/resources/static/scripts/employeeDetail.js
+++ src/main/resources/static/scripts/employeeDetail.js
@@ -3,26 +3,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function validateForm() {
-    // TODO: Validate the user input
-    var first =  document.getElementById('fName').value;
-    var last =  document.getElementById('lName').value;
-    var pass =  document.getElementById('password').value;
-    var confPass =  document.getElementById('confirmPassword').value;
-    var type =  document.getElementById('empType').value;
+    // TODO: Validate the user input and cleanup
+    var first =  getfName();
+    var last =  getlName();
+    var pass =  getPassword();
+    var confPass =  getConfPass();
+    var type =  getEmpType();
     
+    if (first == null){
+        displayError("First name cannot be blank.");
+        return false;
+    }
+    if (last == null){
+        displayError("Last name cannot be blank.");
+        return false;
+    }
+    if (pass == null){
+        displayError("Password cannot be blank.");
+        return false;
+    }
+
     if(pass != confPass) {
         document.getElementById('error1').innerHTML = "Your passwords did not match";
         return false;
     }
 
-    //Check if pass == confPass
+    if (type != 'Cashier' && type != 'Shift Manager' && type != 'General Manager') {
+        displayError("Employee Type not valid.")
+        return false;
+    }
 
     var str = "INSERT INTO employee";
 
 	return true;
 }
-//TODO: Fix from productDetail to EmployeeDetail
-// Save
+
+// TODO: Save
+/*
 function saveActionClick(event) {
 	if (!validateSave()) {
 		return;
@@ -40,6 +57,8 @@ function saveActionClick(event) {
 		lname: getlName(),
 		password: getPassword()
 	};
+//Above still need check
+//Below is unchanged from productDetail
 /*
 	if (firstNameIsDefined) {
 		ajaxPut(saveActionUrl, saveProductRequest, (callbackResponse) => {
@@ -68,85 +87,32 @@ function saveActionClick(event) {
 		});
 	}*/
 };
-/*
-function validateSave() {
-	const lookupCode = getProductLookupCode();
-	if ((lookupCode == null) || (lookupCode.trim() === "")) {
-		displayError("Please provide a valid product lookup code.");
-		return false;
-	}
-
-	const count = getProductCount();
-	if ((count == null) || isNaN(count)) {
-		displayError("Please provide a valid product count.");
-		return false;
-	} else if (count < 0) {
-		displayError("Product count may not be negative.");
-		return false;
-	}
-
-	return true;
-}
-
-function displayProductSavedAlertModal() {
-	if (hideProductSavedAlertTimer) {
-		clearTimeout(hideProductSavedAlertTimer);
-	}
-
-	const savedAlertModalElement = getSavedAlertModalElement();
-	savedAlertModalElement.style.display = "none";
-	savedAlertModalElement.style.display = "block";
-
-	hideProductSavedAlertTimer = setTimeout(hideProductSavedAlertModal, 1200);
-}
-
-function hideProductSavedAlertModal() {
-	if (hideProductSavedAlertTimer) {
-		clearTimeout(hideProductSavedAlertTimer);
-	}
-
-	getSavedAlertModalElement().style.display = "none";
-}*/
 // End save
+
+//TODO:
+/*After save AND employeeID input = hidden,
+    display employeeID input but remain disabled
+*/
 
 // Getters and setters
 function getfName() {
-	return getfNameElement().value;
-}
-function setfName(fName) {
-	getfNameElement().value = fName;
-}
-function getfNameElement() {
 	return document.getElementById("fName");
-}   //First Name
+}//First Name
 
 function getlName() {
-	return getlNameElement().value;
-}
-function setlName(lName) {
-	getlNameElement().value = lName;
-}
-function getlNameElement() {
 	return document.getElementById("lName");
-}   //Last Name
+}//Last Name
 
 function getPassword() {
-	return getPasswordElement().value;
-}
-function setPassword(password) {
-	getPasswordElement().value = password;
-}
-function getPasswordElement() {
 	return document.getElementById("password");
-}   //Password
+}//Password
+
+function getConfPassword() {
+	return document.getElementById("confirmPassword");
+}//Confirm Password
 
 function getEmpType() {
-    return getEmpTypeElement().value;
-}
-function setEmpType(empType) {
-    getEmpType().value = empType;
-}
-function getEmpTypeElement() {
     return document.getElementById("empType");
 }   //Employee Type
+
 // End getters and setters
