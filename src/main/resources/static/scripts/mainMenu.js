eaa7c936a8210b83c3fe587d3797e0a37bd17e24
hj019 <60008915+hj019@users.noreply.github.com>
2020-02-25 21:21:56 -0600

    Add mainMenu.js

--- src/main/resources/static/scripts/mainMenu.js
+++ src/main/resources/static/scripts/mainMenu.js
@@ -1,9 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var errorMessage = "Functionality has not yet been implemented.";
+    document.getElementById("startTxn").addEventListener("click", function()
+    { displayError(errorMessage);});
+    
+    document.getElementById("salesReport").addEventListener("click", function()
+    { displayError(errorMessage);});
+    
+    document.getElementById("cashierReport").addEventListener("click", function()
+    { displayError(errorMessage);});
+    
+    document.getElementById("signOut").addEventListener("click", signOutActionClickHandler());
+});
 
-var errorMessage = "Functionality has not yet been implemented.";
-document.getElementById("startTxn").addEventListener("click", function()
-{ displayError(errorMessage);});
+//!!TODO-Should be defined in a common module that can be accessed 
+//from each of the client-side modules (a view/script(s)/style(s) triplet)
 
 
-// function message(){
-// 	document.getElementById("msg").innerHTML ="Functionality has not yet been implemented.";
-// }
+
