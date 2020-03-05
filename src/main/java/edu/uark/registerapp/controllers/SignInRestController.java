128ebf7c31efa398f77e78f4a4e7e3590a2be381
hj019 <60008915+hj019@users.noreply.github.com>
2020-03-04 23:55:14 -0600

    SignInRestController-Delete

--- src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
@@ -2,11 +2,13 @@ package edu.uark.registerapp.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.uark.registerapp.commands.activeUsers.ActiveUserDeleteCommand;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ApiResponse;
 
@@ -14,13 +16,17 @@ import edu.uark.registerapp.models.api.ApiResponse;
 @RequestMapping(value = "/api")
 public class SignInRestController extends BaseRestController {
 	@RequestMapping(value="/signOut", method = RequestMethod.DELETE)
-	public @ResponseBody ApiResponse removeActiveUser(
-		final HttpServletRequest request
-	) {
-
+	public @ResponseBody ApiResponse removeActiveUser(final HttpServletRequest request)
+	{
+	
+		this.activeUserDeleteCommand.setSessionKey(request.getSession().getId()).execute();
 		// TODO: Sign out the user associated with request.getSession().getId()
 
 		return (new ApiResponse())
 			.setRedirectUrl(ViewNames.SIGN_IN.getRoute());
 	}
+
+
+	@Autowired
+	private ActiveUserDeleteCommand activeUserDeleteCommand;
 }
